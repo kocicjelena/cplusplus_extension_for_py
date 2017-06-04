9616db3e9db57d051fa4905d2ea2dd39b859b5bb
Jelena Kocic <kocicjelena@gmail.com>
2017-06-04 10:34:23 +0200

    Update myimplementation.py

--- myimplementation/myimplementation.py
+++ myimplementation/myimplementation.py
@@ -1,23 +1,7 @@
 import c_module
 import os, sys
 
-def determine_path ():
-    try:
-        root = __file__
-        if os.path.islink (root):
-            root = os.path.realpath (root)
-        return os.path.dirname (os.path.abspath (root))
-    except:
-        print "no root"
-        sys.exit ()
-        
-def run ():
-    modret = c_module.c_module_calc_stat()
-	print modret
-	print determine_path ()
-    print "My c module is in:"
-    files = [f for f in os.listdir(determine_path () + "/myimplementation/*")]
-    print files
+
     
 if __name__ == "__main__":
 c_module.c_module_calc_stat()
