accdafc01f00d585c36da653e142c2ebc5922b17
Jelena Kocic <kocicjelena@gmail.com>
2017-06-04 16:55:12 +0200

    Update myimplementation.py

--- myimplementation/myimplementation.py
+++ myimplementation/myimplementation.py
@@ -9,7 +9,7 @@ class CExtExample:
   def __init__(self):
       pass
   def run(self):
-    print (c_module.c_module_calc_stat())
+    print (c_module.calc_stat())
 def main():
   sample = CExtExample()
   sample.run()
