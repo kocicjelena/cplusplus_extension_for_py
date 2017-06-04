56307d0c1f83fc880187c36a0e8412514fa42964
Jelena Kocic <kocicjelena@gmail.com>
2017-06-04 16:40:51 +0200

    Update myimplementation.py

--- myimplementation/myimplementation.py
+++ myimplementation/myimplementation.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python
+#
+# Copyright 2017 
+#
 import c_module
 import os, sys
+class CExtExample:
+
+  def __init__(self):
+      pass
+  def run(self):
+    print (c_module.c_module_calc_stat())
+def main():
+  sample = CExtExample()
+  sample.run()
 
 
-    
-if __name__ == "__main__":
-c_module.c_module_calc_stat()
+if __name__ == '__main__':
+  main()
